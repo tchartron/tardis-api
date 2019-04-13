0823fc3b9781635a7b8620be3451bc81d2df4957
Thomas Chartron <thomas.chartron@gmail.com>
2019-04-14 01:26:02 +0200

    :rocket: Added start to work button on company show view

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -8,7 +8,9 @@
                 <div class="card-header">Company {{ $company->name }}</div>
 
                 <div class="card-body">
-                    {{ $company->description }}
+                    <div class="my-2">{{ $company->description }}</div>
+                    <a class="d-block btn btn-warning" href="{{ route('companies.index') }}">{{ __('Start to work for') }} {{ $company->name }}</a> {{-- On click we should trigger a vuejs component to track time visually  and change the button to pause / stop--}}
+                    <hr />
                     <a class="d-block" href="{{ route('companies.index') }}">{{ __('Back to companies') }}</a>
                     <a href="{{ route('companies.edit', ['id' => $company->id]) }}" class="btn btn-success" role="button">{{ __('Edit') }}</a>
 
