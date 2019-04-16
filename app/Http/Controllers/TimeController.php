7fdd24d1375203943dfb8b34fe1a118fa1ad5406
Thomas Chartron <thomas.chartron@gmail.com>
2019-04-16 17:39:22 +0200

    :rocket: Creating a Time controller with needed endpoints

---
+++ app/Http/Controllers/TimeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Time;
+use Illuminate\Http\Request;
+
+class TimeController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $times = Time::all();
+        return view('times.index', compact('times'));
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedRequest = request()->validate([
+            'name' => ['required', 'min:3'],
+            'description' => ['required', 'min:10']
+        ]);
+        Time::create($validatedRequest);
+        return back(); // redirect to previous page
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Time  $time
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Time $time)
+    {
+        $time->delete();
+        return back();
+    }
+}
