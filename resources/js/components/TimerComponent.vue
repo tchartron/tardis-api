854a0d399532b5b1aced39f75445931fb316bcd8
Thomas Chartron <thomas.chartron@gmail.com>
2019-04-21 19:10:33 +0200

    :heavy_plus_sign: Added carbon dependency

--- resources/js/components/TimerComponent.vue
+++ resources/js/components/TimerComponent.vue
@@ -23,7 +23,7 @@ export default {
             };
         },
     mounted() {
-        console.log(this.seconds);
+        // console.log({!! json_encode($company->id) !!})
     },
     methods: {
         startTimer() {
