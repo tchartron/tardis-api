17aea033bcf9ecf2e5f8c2b2a7fb9cc5b5012bae
Thomas Chartron <thomas.chartron@gmail.com>
2019-07-30 17:26:05 +0200

    :sparkles: :alien: Gitlab service class

---
+++ app/Http/Controllers/Api/GitlabController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Group;
+use App\Services\Gitlab;
+use League\Container\Container;
+
+class GitlabController extends Controller
+{
+    private $gitlabService;
+
+    public function __construct(Container $container) {
+        if($this->gitlabService === null) {
+            $this->gitlabService = new Gitlab($container);
+        }
+    }
+
+    public function getGroups()
+    {
+        $groups = $this->gitlabService->getGroups();
+        return response()->json($groups);
+    }
+
+    public function getIssuesFromGroup($groupId)
+    {
+        $issues = $this->gitlabService->getIssuesFromGroup($groupId);
+        return response()->json($issues);
+    }
+}
